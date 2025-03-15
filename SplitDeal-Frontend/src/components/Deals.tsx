d539431307be2e0fd01522c010cbbb6261aa65e0
Ayush <indapureayush2003@gmail.com>
2025-03-15 11:20:18 +1100

    deals included

--- SplitDeal-Frontend/src/components/Deals.tsx
+++ SplitDeal-Frontend/src/components/Deals.tsx
@@ -32,12 +32,12 @@ const products = [
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
     {
-        id: 3,
-        name: 'Focus Paper Refill',
+        id: 4,
+        name: 'Machined Mechanical Pencil',
         href: '#',
-        price: '$89',
-        imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-03.jpg',
-        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+        price: '$35',
+        imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
       },
       {
         id: 4,
@@ -48,14 +48,6 @@ const products = [
         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
       },
       {
-        id: 3,
-        name: 'Focus Paper Refill',
-        href: '#',
-        price: '$89',
-        imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-03.jpg',
-        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-      },
-      {
         id: 4,
         name: 'Machined Mechanical Pencil',
         href: '#',
@@ -63,14 +55,14 @@ const products = [
         imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-04-image-card-04.jpg',
         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
       },
+      
     // More products...
   ]
   
-  export default function Example() {
+  export default function Deals() {
     return (
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-
           <h2 className="sr-only">Products</h2>
   
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
